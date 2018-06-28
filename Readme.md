29efee71a09830c6f88618ed29a5801c0b75152a
Dhruvil Patel <pateldhruvil01@yahoo.com>
2018-06-28 10:46:08 -0400

    1st Commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# ARkit App for iOS 
+## Basic House app in World Tracking
+
+	How to use all shapes like:
+		plane
+		pyramid
+		Capsule
+		Cylinder
+		and Many more..
+
+	Used parent Node and child Node
+	
+	How to reset the shape and add it again.
+
+Thank you.
--- WorldTrackingApp/ViewController.swift
+++ WorldTrackingApp/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addBtn(_ sender: Any) {
+        let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
+        doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        let node = SCNNode()
+        // node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+        //        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+        //        let path = UIBezierPath()
+        //        path.move(to: CGPoint(x: 0, y: 0))
+        //        path.addLine(to: CGPoint(x:0, y: 0.2))
+        //        path.addLine(to: CGPoint(x: 0.2, y:0.3))
+        //        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
+        //        path.addLine(to: CGPoint(x: 0.4, y: 0))
+        //        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+        //        node.geometry = shape
+        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        node.position = SCNVector3(0.1,0.1,-0.2)
+        boxNode.position = SCNVector3(0, -0.05, 0)
+        doorNode.position = SCNVector3(0,-0.02,0.053)
+        self.sceneView.scene.rootNode.addChildNode(node)
+        node.addChildNode(boxNode)
+        boxNode.addChildNode(doorNode)
+        //   self.sceneView.scene.rootNode.addChildNode(cylinderNode)
     }
-
-
+    
+    @IBAction func resetBtn(_ sender: Any) {
+        self.restartSession()
+    }
+    
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+    }
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    }
+    
+    
 }
 
